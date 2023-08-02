d25b2dc36b978e8ab911fd9d0b8ab41351b3fe50
bakhtawarneaz <bakhtawarneaz@gmail.com>
2023-08-03 00:53:28 +0500

    login

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,8 +1,63 @@
 import React from 'react'
+import BgImage from '../website/BgImage'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <div className='row mt-80'>
+      <div className='col-8'>
+        <BgImage />
+        {/* <BgImage />
+        <Toaster
+          position='top-right'
+          reverseOrder={false}
+          toastOptions={{
+            style: {
+              fontSize: '14px',
+            },
+          }}
+        /> */}
+      </div>
+      <div className='col-4'>
+        <div className='account'>
+          <div className='account__section'>
+            <form >
+              <div className='group'>
+                <h3 className='form-heading'>Login</h3>
+              </div>
+              <div className='group'>
+                <input
+                  type='email'
+                  name='email'
+                  className='group__control'
+                  placeholder='Enter Email'
+                  // value={state.email}
+                  // onChange={handleInputs}
+                />
+              </div>
+              <div className='group'>
+                <input
+                  type='password'
+                  name='password'
+                  className='group__control'
+                  placeholder='Create Password'
+                  // value={state.password}
+                  // onChange={handleInputs}
+                />
+              </div>
+              <div className='group'>
+                <input
+                  type='submit'
+                  className='btn btn-default btn-block'
+                  value='Login'
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
   )
 }
 
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,8 +1,73 @@
 import React from 'react'
+ import BgImage from '../website/BgImage'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <div className='row mt-80'>
+				<div className='col-8'>
+          <BgImage />
+					{/* <BgImage />
+					<Toaster
+						position='top-right'
+						reverseOrder={false}
+						toastOptions={{
+							style: {
+								fontSize: '14px',
+							},
+						}}
+					/> */}
+				</div>
+				<div className='col-4'>
+					<div className='account'>
+						<div className='account__section'>
+							<form >
+								<div className='group'>
+									<h3 className='form-heading'>Register</h3>
+								</div>
+								<div className='group'>
+									<input
+										type='text'
+										name='name'
+										className='group__control'
+										placeholder='Enter Name'
+										// value={state.name}
+										// onChange={handleInputs}
+									/>
+								</div>
+								<div className='group'>
+									<input
+										type='email'
+										name='email'
+										className='group__control'
+										placeholder='Enter Email'
+										// value={state.email}
+										// onChange={handleInputs}
+									/>
+								</div>
+								<div className='group'>
+									<input
+										type='password'
+										name='password'
+										className='group__control'
+										placeholder='Create Password'
+										// value={state.password}
+										// onChange={handleInputs}
+									/>
+								</div>
+								<div className='group'>
+									<input
+										type='submit'
+										className='btn btn-default btn-block'
+										value='Register'
+									/>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+    </>
   )
 }
 
